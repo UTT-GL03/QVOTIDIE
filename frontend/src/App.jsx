0a68eefa4cbc53285c482d608e4b55034b974b9b
Aurelien Benel <aurelien.benel@utt.fr>
2024-10-25 18:33:39 +0200

    FEATURE: Consult headlines.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,35 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import data from './assets/sample_data.json'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(data)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>
+          <img src={reactLogo}/>
+          QVOTIDIE
+        </h1>
+      </header>
+      <main className="container">
+        {data.articles.map((x, i) =>
+          <Headline {...x} key={i} />
+        )}
+      </main>
     </>
+  )
+}
+
+function Headline({heading, issued, section}) {
+  return (
+    <article>
+      <header>
+        <span className="tag"> {section} </span>
+        <time> {issued} </time>
+      </header>
+      <h2>{heading}</h2>
+    </article>
   )
 }
 
