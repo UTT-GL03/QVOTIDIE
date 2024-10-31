4518e8598515d995518624a9ab68441c1a025df3
Aurelien Benel <aurelien.benel@utt.fr>
2024-10-31 18:07:23 +0100

    REQUIREMENT: Data should be loaded dynamically.
    # Conflicts:
    #	frontend/src/Article.jsx
    #	frontend/src/Headlines.jsx

--- frontend/src/Headlines.jsx
+++ frontend/src/Headlines.jsx
@@ -1,16 +1,27 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/fr'
-import data from './assets/sample_data.json'
 
 dayjs.extend(relativeTime)
 dayjs.locale('fr')
 
 function Headlines() {
-  const articlesByRow = Object.values(
-    Object.groupBy(data.articles, (x, i) => Math.floor(i/3))
-  )
+  const [articlesByRow, setArticlesByRow] = useState([])
+
+  useEffect(() => {
+    fetch('/sample_data.json')
+      .then(x => x.json())
+      .then(data => {
+        setArticlesByRow(
+          Object.values(
+            Object.groupBy(data.articles, (x, i) => Math.floor(i/3))
+          )
+        )
+      })
+  }, [])
+
   return (
     <main className="container">
       {articlesByRow.map((x, i) =>
